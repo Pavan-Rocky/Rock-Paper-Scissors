7f942bce2b06c68a994bca48dac5fffa2d9b53c2
Pavan-Rocky <pavankumarpc14@gmail.com>
2025-08-07 13:31:06 +0530

    added count for this game

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
- function playGame(PlayerMove){
+let score=JSON.parse(localStorage.getItem('score')) || {
+    wins :0,
+    looses :0,
+    ties :0
+};
+
+// if (score===null) { // if(!score) because its a falsy value or else we use OR oparator
+//     score = {
+//         wins :0,    // because we use OR operator
+//         looses :0,
+//         ties :0
+//     }
+// }
+
+
+function playGame(PlayerMove){
             const computerMove = pickComputerMove();
+
+            console.log();
+            
     
             let result ='';
            if (PlayerMove==='scissor') {
@@ -35,23 +53,36 @@
                     result='win';
                 }
            }
-            alert(`your move ${PlayerMove} the Computer move is ${computerMove} and the result is ${result}`);
-        }
+
+           if(result==='win'){
+            score.wins +=1;
+           }
+           else if (result==='loose') {
+            score.looses+=1;
+           }
+           else if (result==='tie') {
+            score.ties +=1;
+           }
+
+           localStorage.setItem('score',JSON.stringify(score));
 
 
-        function pickComputerMove(){
-            let computerMove='';
-            const randomNumber= Math.random();
-            console.log(randomNumber);
-            if(randomNumber>=0 && randomNumber<=1/3){
-                computerMove=`rock`;
-            }
-            else if(randomNumber>1/3 && randomNumber<=2/3){
-                computerMove=`paper`;
-            }
-            else if(randomNumber>2/3 && randomNumber<1){
-                computerMove=`scissor`;
-            }
-            console.log(computerMove); 
-            return computerMove;
+            alert(`your move ${PlayerMove} the Computer move is ${computerMove} and the result is ${result} \n Wins : ${score.wins}  Looses : ${score.looses}  Ties : ${score.ties}` );
+}
+
+function pickComputerMove(){
+    let computerMove='';
+    const randomNumber= Math.random();
+    console.log(randomNumber);
+        if(randomNumber>=0 && randomNumber<=1/3){
+            computerMove=`rock`;
         }
+        else if(randomNumber>1/3 && randomNumber<=2/3){
+            computerMove=`paper`;
+        }
+        else if(randomNumber>2/3 && randomNumber<1){
+            computerMove=`scissor`;
+        }
+        console.log(computerMove); 
+    return computerMove;
+}
